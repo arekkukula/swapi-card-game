b01eaf5c95b073d267360817b0b4fb984319c219
arekkukula <arkadiusz.kukula@gmail.com>
2023-07-11 16:27:12 +0200

    Dirty game prototype

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { DataService } from './core/services/data.service';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
-  }));
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AppComponent],
+      providers: [
+        { provide: DataService, useValue: jasmine.createSpyObj('DataService', ['']) },
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+    }).compileComponents();
+
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
